f564bf0044279c26219b72c8e551554024719d53
Zoltan Kiss <zoltan.a.kiss@gmail.com>
2023-01-09 02:44:48 +0100

    improve testing

--- test/integration/nhl_live_game_stat_processor_test.rb
+++ test/integration/nhl_live_game_stat_processor_test.rb
@@ -11,6 +11,21 @@ class NhlLiveGameStatProcessorTest < ActionDispatch::IntegrationTest
 
     NhlLiveGameStatProcessor.new(JSON.parse(File.read(Rails.root.join('test', 'json', 'live_feed.json'))), 2022020639).process
 
-    assert NhlGame.count > 0
+    assert_equal 8, NhlGame.count
+    assert_equal 46, NhlPlayerGameStat.count
+
+    player = NhlPlayerGameStat.where(player_id: "ID8471675").first
+
+    assert_equal "ID8471675", player.player_id
+    assert_equal 2022020639, player.nhl_game_id
+    assert_equal "Sidney Crosby", player.player_name
+    assert_equal "5", player.team_id
+    assert_equal "Pittsburgh Penguins", player.team_name
+    assert_equal "35", player.player_age
+    assert_equal "87", player.player_number
+    assert_equal "Center", player.player_position
+    assert_equal 2, player.assists
+    assert_equal 0, player.goals
+    assert_equal 0, player.hits
   end
 end
